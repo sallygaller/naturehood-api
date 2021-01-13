e77702cf7afea0a5bde3df2c1860ea459a29103b
unknown <sallyannegaller@gmail.com>
2021-01-12 16:36:45 -0800

    Deploying app to heroku - changed env variable namnes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://postgres@localhost/naturehood",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://postgres@localhost/naturehood",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://postgres@localhost/naturehood-test",
 };
