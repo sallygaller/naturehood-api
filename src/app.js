501a90b656a9bfdf2113055097ada56fafa28ceb
unknown <sallyannegaller@gmail.com>
2021-01-25 14:56:43 -0800

    Changed CORS header

--- src/app.js
+++ src/app.js
@@ -14,11 +14,12 @@ const app = express();
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN,
-  })
-);
+// app.use(
+//   cors({
+//     origin: CLIENT_ORIGIN,
+//   })
+// );
+app.use(cors());
 app.use(helmet());
 
 app.use("/api/observations", observationsRouter);
